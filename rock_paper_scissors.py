508ec669b56cc15675e287cfe131e1ba419a5707
Zelkann <84004274+Zelkann@users.noreply.github.com>
2022-03-18 05:09:42 +0800

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,10 +1,12 @@
 import random
 
 
-player1 = input("Enter a hand gesture (rock, paper, scissors): ")
+# Players simultaneously choose a hand
+player1 = input("Enter a hand gesture (rock, paper, scissors): ").lower()
 player2 = random.choice(["rock", "paper", "scissors"])
 print(f"Player 2 chose {player2}")
 
+# Determine Winner
 if player1 == player2:
     print("It's a tie!")
 elif (player1, player2) in [("rock", "scissors"), ("scissors", "paper"), ("paper", "rock")]:
